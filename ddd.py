67b24f627ddf1945c18e92aed8f2a2223e2ace99
BillJung1024 <bestmrs1024@naver.com>
2016-03-29 23:13:15 +0900

    바람개비만들기
    201611104

---
+++ ddd.py
@@ -0,0 +1,11 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def varamgebi(size,bigger,turns,sides,angle):
+    for i in range(0,sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+varamgebi(30,20,10,50,90)
+wn.exitonclick()
