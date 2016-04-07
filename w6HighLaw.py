bf5c436748a97454cc9e2c25a54df3bb487c6844
BillJung1024 <bestmrs1024@naver.com>
2016-04-07 18:35:41 +0900

    w6HighLow.py

---
+++ w6HighLaw.py
@@ -0,0 +1,19 @@
+﻿import random
+guessesTaken = 0
+
+print('Hello! What is your name?')
+myName = input()
+number = random.randint(1,100)
+print('Well, ' + myName + ', I am thinking of a number between 1 and 100.')
+while guessesTaken < 6:
+    print('Take a guess.')
+    guess = input()
+    guess = int(guess)
+    
+    guessesTaken = guessesTaken + 1
+    if guess < number:
+        print ("Your guess is too low.")
+    if guess > number:
+        print ("yout guess is too high.")
+    if guess == number:
+        break
