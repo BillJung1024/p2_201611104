b12ef78d2f4d4779b457e55f804c8cf1901bf4ba
BillJung1024 <bestmrs1024@naver.com>
2016-04-11 17:18:53 +0900

    4-11 과제입니다..

---
+++ w6Main1.py
@@ -0,0 +1,18 @@
+﻿def sumList(aList):
+    y=list()
+    for i in range(0,aList):
+        if i%4==0 and i%5!=0:
+            y.append(i)
+    sum=0
+    for i in range(0,len(y)):
+        sum+=y[i]
+    mysum=sum
+    return mysum
+def lab6():
+    "**** Programing"
+    aList=1000
+    labsum=sumList(aList)
+    print labsum   
+def main():
+    lab6()
+main()
