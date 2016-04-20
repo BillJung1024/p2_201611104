fab90c2df7a479f63ea938b8ef57e6170403b2d2
BillJung1024 <bestmrs1024@naver.com>
2016-04-20 13:34:40 +0900

    제어문제2&문제3

---
+++ w7Main2.py
@@ -0,0 +1,36 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+x=list()
+
+def drawSquareAt(size, pos): 
+    t1.penup() 
+    t1.setpos(size)
+    t1.pendown() 
+    for i in range(0,4):
+        tracks=append(t1.pos())
+        t1.forward(size) 
+        t1.right(90) 
+    return tracks
+
+def lab7a():
+    mytrack=drawSquareAt(size,pos)
+    print mytrack
+
+import turtle 
+wn=turtle.Screen() 
+t1=turtle.Turtle() 
+c=dict() 
+c=({0:(0,0),1:(100,0),2:(100,100),3:(0,100),4:(0,0)}) 
+
+def lab7b(): 
+	for i in range(1,5): 
+		t1.goto(c[i]) 
+	for j in range(1,5): 
+ 		print c[j] 
+    
+    
+def main():
+    lab7a()
+    lab7b()
+    
