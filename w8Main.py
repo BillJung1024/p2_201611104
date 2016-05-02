c0ddddd2297745e39e657a0223328135de3c62da
BillJung1024 <bestmrs1024@naver.com>
2016-05-02 15:12:15 +0900

    문자 갯수 새기

---
+++ w8Main.py
@@ -0,0 +1,26 @@
+﻿def charCount():
+	sentence='sangmyung university'
+	d=dict()
+	for c in word:
+	    if c not in d:
+	        d[c]=1
+	    else:
+	        d[c]=d[c]+1
+        
+	%matplotlib inline 
+
+	import matplotlib
+	import matplotlib.pyplot as plt
+
+	plt.bar( range(len(d)),d.values(), align='center')
+	plt.xticks(range(len(d)), list(d.keys()))
+	plt.show()
+
+def lab8():
+	charCount()
+
+def main():
+	lab8()
+
+if__name__=="__main__":
+	main()
