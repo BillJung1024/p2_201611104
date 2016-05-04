07d2baf3ae6d558be45e6d6cda1dbfcbe9dc4d73
BillJung1024 <bestmrs1024@naver.com>
2016-05-04 13:53:05 +0900

    제일 가까운 역 찾기

--- w9Main.py
+++ w9Main.py
@@ -1,9 +1,18 @@
-﻿import math
-Locations=tuple()
-myList=list()
+﻿def lab9():
+	import math
+	Locations=tuple()
+	myList=list()
 
-(x1,y1)=(37.575869, 126.976637)
-Locations=[(37.576549, 126.98552), (37.571618, 126.976551), (37.574577, 126.957754)]
-for i in Locations:
-    myList.append(math.sqrt((x1-i[0])**2+(y1-i[1])**2))
-print min(myList)
+	(x1,y1)=(37.575869, 126.976637)
+	Locations=[(37.576549, 126.98552), (37.571618, 126.976551), (37.574577, 	126.957754)]
+	for i in Locations:
+	    myList.append(math.sqrt((x1-i[0])**2+(y1-i[1])**2))
+	print min(myList)
+
+def main():
+	lab9()
+
+
+
+
+	
