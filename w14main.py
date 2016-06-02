6154231b24ea4b92cf9795b61959e447582ae395
BillJung1024 <bestmrs1024@naver.com>
2016-06-02 16:46:53 +0900

    Add files via upload

---
+++ w14main.py
@@ -0,0 +1,23 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "My dog name is ",self.name
+        
+    def talk(self,name):
+        print "mung mung"
+        
+class ShihTzuDog(object):
+    def talk(self,name):
+        print "si si"
+
+class Maltese(object):
+    def talk(self,name):
+        print "mal mal"
+mydog.getName()
+mydog.talk('dog')
+Bill=ShihTzuDog()
+Bill.talk('ShihTzuDog')
+Mike=Maltese()
+Mike.talk('Maltese')
+raw_input("dogs")
